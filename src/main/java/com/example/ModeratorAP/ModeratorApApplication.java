616491c4ba65a4ea36cb8b428ae64eda955fe588
Rendy <mrsendyuk@gmail.com>
2018-11-21 15:58:47 +0200

    upload

---
+++ src/main/java/com/example/ModeratorAP/ModeratorApApplication.java
@@ -0,0 +1,12 @@
+package com.example.ModeratorAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeratorApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeratorApApplication.class, args);
+	}
+}
